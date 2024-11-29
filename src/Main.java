a747a1628ea1c84ddb02b768952ba4568b90bcea
havokkgittests <havokkmorandsforgames@gmail.com>
2024-11-29 13:44:46 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import model.Pessoa;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Author: 
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<Pessoa> listPessoas = Pessoa.listPessoa();
+
+        System.out.println("Masculinos: ");
+        List<Pessoa> listMasculino = listPessoas.stream()
+                .filter(pessoa -> pessoa.getGenero().equals("Masculino"))
+                .collect(Collectors.toList());
+
+        listMasculino.forEach(System.out::println);
+
+        System.out.println("---------------------");
+
+        System.out.println("Femininos: ");
+        listPessoas.stream()
+                .filter(pessoa -> pessoa.getGenero().equals("Feminino"))
+                .forEach(System.out::println);
+    }
+}
